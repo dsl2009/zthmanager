1cd6e7e1498c3db322f732ef492e1b17cc54c385
dsl <dsl_gree@163.com>
2020-01-15 10:41:08 +0800

    apiinit

--- ztu/data_manager.py
+++ ztu/data_manager.py
@@ -120,7 +120,14 @@ def get_history(dev_mac):
     cursor.execute(sql,(dev_mac,))
     results = cursor.fetchall()
     db.close()
+    data = []
     dts = {'data':[]}
     for x in results:
-        dts['data'].append([x[1].timestamp(),x[0]+10])
+        data.append([x[1].timestamp(),x[0]+10])
+    if len(data)>2:
+        for k in range(len(data)-1):
+            dts['data'].append(data[k])
+            dts['data'].append([data[k+1][0]-1,data[k][1]])
+    else:
+        dts['data'] = data
     return dts
